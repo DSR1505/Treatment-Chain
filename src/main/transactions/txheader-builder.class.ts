acb20c8cba7c2adc9cccc488abb924f2a81a2914
aayushbest <thebest1@live.in>
2021-01-21 17:48:19 +0530

    started created txheaders

--- src/main/transactions/txheader-builder.class.ts
+++ src/main/transactions/txheader-builder.class.ts
@@ -1,4 +1,7 @@
-export default interface TransactionHeaderBuilder {
+import { read } from "fs";
 
-
+export default class transactionHeader {
+    public readonly timestamp: number = new Date().getTime();
+    private _transactionIdentifier: number;
+    private _patientIdentifier: string;
 }
